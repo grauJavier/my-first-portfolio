6ba4e297dbd3bc680f6d2dddfad65f720c671b89
Javier Grau <javiergrau.freelance@gmail.com>
2023-05-17 16:23:54 -0400

    Add Pop-Up window responding to their project

--- script.js
+++ script.js
@@ -8,29 +8,29 @@ hiddenMenuBg.style.display = "none";
 hiddenMenu.style.display = "none";
 
 function openMenu() {
-    hiddenMenuBg.style.display = "flex";
-    hiddenMenu.style.display = "grid";
-    setTimeout(function () {
-        hiddenMenuBg.style.opacity = 0.7;
-        hiddenMenu.style.opacity = 1;
-    }, 10);
+  hiddenMenuBg.style.display = "flex";
+  hiddenMenu.style.display = "grid";
+  setTimeout(function () {
+    hiddenMenuBg.style.opacity = 0.7;
+    hiddenMenu.style.opacity = 1;
+  }, 10);
 }
 
 function closeMenu() {
-    hiddenMenuBg.style.opacity = 0;
-    hiddenMenu.style.opacity = 0;
-    setTimeout(function () {
-        hiddenMenuBg.style.display = "none";
-        hiddenMenu.style.display = "none";
-    }, 500);
+  hiddenMenuBg.style.opacity = 0;
+  hiddenMenu.style.opacity = 0;
+  setTimeout(function () {
+    hiddenMenuBg.style.display = "none";
+    hiddenMenu.style.display = "none";
+  }, 500);
 }
 
 function display() {
-    if (hiddenMenu.style.display == "none") {
-        openMenu();
-    } else {
-        closeMenu();
-    }
+  if (hiddenMenu.style.display == "none") {
+    openMenu();
+  } else {
+    closeMenu();
+  }
 }
 
 menuBtn.addEventListener("click", display);
@@ -38,81 +38,111 @@ closeBtn.addEventListener("click", display);
 sectionBtn.addEventListener("click", display);
 window.addEventListener("resize", closeMenu);
 
-let modalPopupContainer = document.querySelector("#modal-popup-container");
+// MODAL POPUP ANIMATION
 
-function htmlBuilder() {
-    modalPopupContainer.insertAdjacentHTML(
-        "afterbegin",
-        `
+let modalPopupContainer = document.querySelector("#modal-popup-container");
+let arrContent = [];
+
+function objBuilder(title, client, role, year, img_src, body_text, tags) {
+  let newObj = new Object();
+  arrContent.push(newObj);
+
+  let i = arrContent.length - 1;
+
+  arrContent[i].title = title;
+  arrContent[i].client = client;
+  arrContent[i].role = role;
+  arrContent[i].year = year;
+  arrContent[i].img_src = img_src;
+  arrContent[i].body_text = body_text;
+  arrContent[i].tags = tags;
+}
+
+objBuilder(
+  "Tonic",
+  "CANOPY",
+  "Back End Dev",
+  "2015",
+  "./visuals/tonic-snapshot.png",
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  ["HTML", "CSS", "JavaScript"]
+);
+
+objBuilder(
+  "Multi-Post Stories",
+  "CANOPY",
+  "Back End Dev",
+  "2015",
+  "./visuals/multi-post-stories-snapshot.png",
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  ["HTML", "CSS", "JavaScript"]
+);
+
+objBuilder(
+  "Tonic",
+  "CANOPY",
+  "Back End Dev",
+  "2015",
+  "./visuals/tonic-snapshot-02.png",
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  ["HTML", "CSS", "JavaScript"]
+);
+
+objBuilder(
+  "Multi-Post Stories",
+  "CANOPY",
+  "Back End Dev",
+  "2015",
+  "./visuals/multi-post-stories-snapshot-02.png",
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  ["HTML", "CSS", "JavaScript"]
+);
+
+function htmlBuilder(index) {
+  modalPopupContainer.insertAdjacentHTML(
+    "afterbegin",
+    `<div id="popup-background"></div>
     <div id="work-card-popup">
-      <h2 class="work-title-text">Tonic</h2>
+      <h2 class="work-title-text">${arrContent[index].title}<i id="close-btn" class="bi bi-x"></i></h2>
       <ul class="client-role-year">
-        <li class="work-client">CANOPY</li>
+        <li class="work-client">${arrContent[index].client}</li>
         <span class="bullet">•</span>
-        <li class="work-role">Back End Dev</li>
+        <li class="work-role">${arrContent[index].role}</li>
         <span class="bullet">•</span>
-        <li class="work-year">2015</li>
+        <li class="work-year">${arrContent[index].year}</li>
       </ul>
-      <img class="work-snapshot" src="./visuals/tonic-snapshot.png" />
+      <img class="work-snapshot" src="${arrContent[index].img_src}" />
       <p class="body-text">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only
-        five centuries, but also the leap into electronic typesetting,
-        remaining essent
+      ${arrContent[index].body_text}
       </p>
       <ul class="tag-list">
-        <li class="tag">HTML</li>
-        <li class="tag">CSS</li>
-        <li class="tag">JavaScript</li>
+        <li class="tag">${arrContent[index].tags[0]}</li>
+        <li class="tag">${arrContent[index].tags[1]}</li>
+        <li class="tag">${arrContent[index].tags[2]}</li>
       </ul>
       <hr class="work-card-popup-line"></hr>
       <div class="work-buttons-container">
         <a class="button button-popup button-see-live" id="see-live-1">See live
-          <i class="bi bi-arrow-up-right-circle" style="min-height: 18px"></i></a>
+          <i class="bi bi-arrow-up-right-circle"></i></a>
         <a class="button button-popup" id="see-source-1">See source <i class="bi bi-github" style="min-height: 18px"></i></a>
       </div>
-      <button class="close-button">Close</button>
-    </div>
-    <div id="popup-container"></div>`
-    );
+    </div>`
+  );
 
-    modalPopupContainer.classList.add("show");
+  // BUTTON INTERACTION IN WORK CARD
+  modalPopupContainer.classList.add("show");
 
-    const closeButton = document.querySelector(".close-button");
-    closeButton.addEventListener("click", () => {
-        modalPopupContainer.classList.remove("show");
-        modalPopupContainer.innerHTML = "";
-    });
+  const closeButton = document.querySelector("#work-card-popup #close-btn");
+  closeButton.addEventListener("click", () => {
+    modalPopupContainer.classList.remove("show");
+    modalPopupContainer.innerHTML = "";
+  });
 }
-let buttons = document.querySelectorAll(".button");
 
-buttons.forEach((button) => {
-    button.addEventListener("click", htmlBuilder);
+let buttons = document.querySelectorAll(".work-card .button");
+
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    htmlBuilder(index);
+  });
 });
-
-// // Modal pop-up section
-// let properties = {
-//   "align-self": "center",
-//   "justify-self": "center",
-//   "height": "100vh",
-//   "width": "100vw",
-//   "z-index": 3,
-//   "position": "fixed",
-//   "top": 0,
-//   "bottom": 0,
-//   "left": 0,
-//   "right": 0,
-//   "display": "flex",
-//   "background-color": "#000",
-// };
-
-// function htmlBuilder(id, obj) {
-//   for (let i = 0; i < Object.keys(obj); i++) {
-//     console.log(Object.keys(obj)[i] + ": " + Object.values[i]);
-//   }
-// }
-
-// let modalContainer = document.querySelector("#modal-pop-up-container");
-//
